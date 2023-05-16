65cc62259f6150db75bb6c0129cbf70b7b0b22aa
Matheus <matheus200211@gmail.com>
2023-05-15 21:52:11 -0300

    Adding the conditions for the search, what is going to bring, starting the svg of the image, and adjusting the url.

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ let result = document.getElementById("result");
 
 function receiveMovie(){
     let movineName = refMovieName.value;
-    
+    let url = `https://www.omdbapi.com/?t=${movineName}$apikey${key}`;
     //if field is empty
 
     if(movineName.length <=0){
         result.innerHTML = '<h3 class="msg">Please enter a movie name </h3>';
     }
 
+
+    //if field isn't empty
+    else{
+        fetch(url).then((resp) => resp.json()).then((data) =>{
+            //if exist in db 
+            if(data.response == "True"){
+                result.innerHTML = `
+                <div class="info">
+                    <img src=${data.Poster} class="poster">
+                    </div>
+                        <h2>${data.Title}</h2>
+                        <div class="rating">
+                            <img src="star.svg"> 
+                            <h4>${data.imdbRating}</h4>
+
+                `
+
+            }
+        })
+    }
 }
