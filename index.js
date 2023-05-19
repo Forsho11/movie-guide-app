e93e520d0ebc99a645adc1ff4074559ab3ed0166
Matheus <matheus200211@gmail.com>
2023-05-18 21:31:19 -0300

    Last adjusment in the api key, finalization of the javascript code, now have events, conditions if have erros etc. In the finalization of the css, to adjust the elements in the web page.

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let result = document.getElementById("result");
 
 //Get data from API.
 
-function receiveMovie(){
-    let movineName = refMovieName.value;
-    let url = `https://www.omdbapi.com/?t=${movineName}$apikey${key}`;
+let receiveMovie = () => {
+    let movieName = refMovieName.value;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    
     //if field is empty
 
-    if(movineName.length <=0){
+    if(movieName.length <= 0){
         result.innerHTML = '<h3 class="msg">Please enter a movie name </h3>';
     }
 
@@ -18,19 +19,45 @@ function receiveMovie(){
     else{
         fetch(url).then((resp) => resp.json()).then((data) =>{
             //if exist in db 
-            if(data.response == "True"){
+            if(data.Response == "True"){
                 result.innerHTML = `
-                <div class="info">
-                    <img src=${data.Poster} class="poster">
-                    </div>
-                        <h2>${data.Title}</h2>
-                        <div class="rating">
-                            <img src="star.svg"> 
-                            <h4>${data.imdbRating}</h4>
-
-                `
-
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star.svg"> 
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3>Plot:</h3>
+                        <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                        <p>${data.Actors}</p>   
+                `;
+            } 
+            
+            //movie doesn't exist in DB
+            else{ 
+                
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
             }
         })
+        //error occurs
+        .catch(() =>{
+            result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+        });
     }
-}
+};
+
+searchBtn.addEventListener("click", receiveMovie);
+window.addEventListener("load", receiveMovie);
